909f88e7fffeb66b6ee02b79ab835ff39a4f3c65
KTK <ktk@KTKs-MacBook-Pro.local>
2022-12-21 23:39:07 +1100

    react init

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@
 /* Building a resume using ReactJS */
 
 // Importing the React library
-import React from 'react';
-// Importing the ReactDOM library
-import ReactDOM from 'react-dom';
-// Importing the App component
-import App from './App';
+import React from "react";
+
+import Button from "react-bootstrap/Button";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+/* The following line can be included in your src/index.js or App.js file */
+
+import "./index.scss";
+
+import "@custom";
 
